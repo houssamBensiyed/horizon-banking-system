a641af622a684d12b70edc27f2f85d915dccdf58
Houssam Bensiyed <med.ab02017@gmail.com>
2025-10-07 20:14:57 +0100

    Implement the HeaderBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Houssam" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account and transactions efficiently"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
